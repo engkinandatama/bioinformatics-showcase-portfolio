0e636368760c5cb7671559edb53de1b915affd8f
nanda_en <engkinandatama@outlook.com>
2025-04-27 15:29:29 +0700

    feat: content/skills

---
+++ src/content/skills-section.ts
@@ -0,0 +1,6 @@
+import { SkillsSectionContent } from './types';
+
+export const skillsSectionContent: SkillsSectionContent = {
+  title: "Skills & Expertise",
+  subtitle: "Technologies and tools I work with."
+};
