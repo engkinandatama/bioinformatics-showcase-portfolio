1790b3cbca860d8c07faadd596bbde93ced94d66
nanda_en <engkinandatama@outlook.com>
2025-04-27 15:27:42 +0700

    feat: content/publication

---
+++ src/content/publications-section.ts
@@ -0,0 +1,20 @@
+import { PublicationsSectionContent } from './types';
+
+export const publicationsSectionContent: PublicationsSectionContent = {
+  title: "Publications & Blog",
+  subtitle: "My research publications and technical writing",
+  tabs: {
+    blog: "Blog Posts",
+    publications: "Publications"
+  },
+  buttons: {
+    viewAllPosts: "View All Posts",
+    viewAllPublications: "View All Publications"
+  },
+  labels: {
+    readArticle: "Read Article",
+    viewPaper: "Paper",
+    viewPatent: "Patent",
+    viewPublication: "Publication"
+  }
+};
