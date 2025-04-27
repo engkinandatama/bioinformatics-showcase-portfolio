dc5a2a488b5f48834c21a572d6836abb978d55ca
nanda_en <engkinandatama@outlook.com>
2025-04-27 15:25:36 +0700

    feat: content/header

---
+++ src/content/header.ts
@@ -0,0 +1,10 @@
+import { NavLink } from './types';
+
+export const navLinks: NavLink[] = [
+  { name: 'Home', href: '/#hero' },
+  { name: 'About', href: '/#about' },
+  { name: 'Experience', href: '/#experience' },
+  { name: 'Projects', href: '/#projects' },
+  { name: 'Publications', href: '/#publications' },
+  { name: 'Contact', href: '/#contact' },
+];
