d30e052fb13424d50f20b3babb055ee8aef61cfb
nanda_en <engkinandatama@outlook.com>
2025-04-27 15:23:17 +0700

    feat: content/education

---
+++ src/content/education.ts
@@ -0,0 +1,60 @@
+import { Education } from './types';
+
+export const educationHistory: Education[] = [
+  {
+    degree: 'Master of Science in Data Science',
+    institution: 'Global Data University',
+    period: '2023-2025',
+    thesis: 'Predictive Modeling in Urban Farming Using Environmental Sensors',
+    coursework: [
+      'Big Data Analytics',
+      'Statistical Learning',
+      'Sensor Data Analysis',
+      'Python for Data Science',
+      'Time Series Forecasting',
+      'AI in Agriculture'
+    ]
+  },
+  {
+    degree: 'Bachelor of Science in Bioinformatics',
+    institution: 'XYZ University',
+    period: '2020-2024',
+    thesis: 'Exploring Protein-Ligand Interactions Using Computational Tools',
+    coursework: [
+      'Molecular Biology',
+      'Genomics',
+      'Data Structures',
+      'Machine Learning',
+      'Bioinformatics Algorithms',
+      'Statistical Methods'
+    ]
+  },
+  {
+    degree: 'Bachelor of Engineering in Computer Science',
+    institution: 'Techno Institute of Technology',
+    period: '2019-2023',
+    thesis: 'Optimizing IoT Communication for Smart Agriculture Systems',
+    coursework: [
+      'Internet of Things',
+      'Embedded Systems',
+      'Algorithms and Complexity',
+      'Wireless Networks',
+      'AgriTech Solutions',
+      'Cloud Computing'
+    ]
+  },
+  {
+    degree: 'Bachelor of Science in Biotechnology',
+    institution: 'BioNext Institute',
+    period: '2018-2022',
+    thesis: 'CRISPR-based Genome Editing and its Ethical Implications',
+    coursework: [
+      'Cell Biology',
+      'Genetic Engineering',
+      'Ethics in Biotechnology',
+      'Systems Biology',
+      'Computational Biology',
+      'Organic Chemistry'
+    ]
+  }
+];
