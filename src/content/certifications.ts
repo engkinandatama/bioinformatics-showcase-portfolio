6f3e23afd15f6007c60b853f93815e2a8c884848
nanda_en <engkinandatama@outlook.com>
2025-04-27 15:21:28 +0700

    feat: content/certifications

---
+++ src/content/certifications.ts
@@ -0,0 +1,88 @@
+import { Certification } from './types';
+
+export const certifications: Certification[] = [
+  {
+    name: 'HarvardX CS50: Introduction to Computer Science',
+    issuer: 'edX / Harvard University',
+    date: 'February 2024',
+    link: 'https://cs50.harvard.edu/certificates/verify/cs50abc'
+  },
+  {
+    name: 'Microsoft Certified: Azure Fundamentals',
+    issuer: 'Microsoft',
+    date: 'January 2024',
+    link: 'https://learn.microsoft.com/en-us/certifications/verify/azure-fundamentals'
+  },
+  {
+    name: 'IBM Data Science Professional Certificate',
+    issuer: 'IBM',
+    date: 'November 2023',
+    link: 'https://www.credly.com/ibm-datasci-cert/verify/ibm456'
+  },
+  {
+    name: 'TensorFlow Developer Certificate',
+    issuer: 'TensorFlow',
+    date: 'September 2023',
+    link: 'https://www.tensorflow.org/certificate/verify/tfdev456'
+  },
+  {
+    name: 'Certified Ethical Hacker (CEH)',
+    issuer: 'EC-Council',
+    date: 'July 2023',
+    link: 'https://aspen.eccouncil.org/verify/ceh789'
+  },
+  {
+    name: 'Google Associate Cloud Engineer',
+    issuer: 'Google Cloud',
+    date: 'June 2023',
+    link: 'https://cloud.google.com/certification/verify/67890'
+  },
+  {
+    name: 'AWS Certified Cloud Practitioner',
+    issuer: 'Amazon Web Services',
+    date: 'May 2023',
+    link: 'https://aws.amazon.com/certification/certified-cloud-practitioner/'
+  },
+  {
+    name: 'Oracle Cloud Infrastructure Foundations',
+    issuer: 'Oracle',
+    date: 'May 2023',
+    link: 'https://education.oracle.com/verify/oci101'
+  },
+  {
+    name: 'AWS Certified Developer',
+    issuer: 'Amazon Web Services',
+    date: 'March 2023',
+    link: 'https://aws.amazon.com/verification/12345'
+  },
+  {
+    name: 'Python for Data Science',
+    issuer: 'Coursera',
+    date: 'January 2023',
+    link: 'https://www.coursera.org/specializations/python-data-science'
+  },
+  {
+    name: 'CompTIA Security+',
+    issuer: 'CompTIA',
+    date: 'December 2022',
+    link: 'https://www.comptia.org/certifications/verify/securityplus123'
+  },
+  {
+    name: 'Machine Learning Specialization',
+    issuer: 'Coursera',
+    date: 'December 2022',
+    link: 'https://coursera.org/verify/specialization/ML123'
+  },
+  {
+    name: 'Introduction to Bioinformatics',
+    issuer: 'edX',
+    date: 'September 2022',
+    link: 'https://www.edx.org/learn/bioinformatics'
+  },
+  {
+    name: 'Cisco Certified Network Associate (CCNA)',
+    issuer: 'Cisco',
+    date: 'April 2022',
+    link: 'https://www.cisco.com/c/en/us/training-events/training-certifications/verify.html'
+  }
+];
